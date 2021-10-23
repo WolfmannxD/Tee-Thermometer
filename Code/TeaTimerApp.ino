589a29420b47459a74bfe823cefeb28e71b3d541
Wolfgang Breu <wolfgang.breu@outlook.de>
2021-10-24 01:31:07 +0200

    implemented basic serial functions

--- Code/TeaTimerApp.ino
+++ Code/TeaTimerApp.ino
@@ -5,6 +5,8 @@
 #include <Wire.h>
 #include "SSD1306Ascii.h"
 #include "SSD1306AsciiWire.h"
+#include <iostream>
+#include <string>
 // #include <ESP8266WiFi.h>  // macht immer noch Probleme ...
 // #include <PubSubClient.h>
 
@@ -12,13 +14,24 @@
 
 // State of the system
 bool SERIAL_ACTIVE{false};
+SerialTempInput dataInput;
+SerialInterface serial;
 
 // the setup function is run once after startup
 void setup() {
-    SerialInterface serial = SerialInterface(); // Serial.begin etc. inside constructor
+    serial = SerialInterface(); // Serial.begin etc. inside constructor
     SERIAL_ACTIVE = serial.initSuccessful;
+    if (SERIAL_ACTIVE)
+    {
+        dataInput = SerialTempInput(serial);
+    };
 }
 
 // the loop function is looped indefinitely after setup
 void loop() {
+    static double T;
+    serial.handleSerial();
+    T = dataInput.getTemperature();
+    delay(5000);
+    serial.write_str(String(T));
 }
--- Code/include/TeaTimer.h
+++ Code/include/TeaTimer.h
@@ -1,11 +1,14 @@
 #ifndef _TEATIMER_H_
 #define _TEATIMER_H_
 // #include <Arduino.h>
+#include <iostream>
+#include <queue>
+// #include <stdlib.h>
 
 class TeaTimer;          // main class
 class Parameters;        // hold config parameters
 class TemperatureSensor; // provides interface to sensor
-class SerialInput;       // simulates sensor with received data
+class SerialTempInput;       // simulates sensor with received data
 class SerialOutput;      // print output over serial
 class SerialInterface;   // handles serial communication
 class MQTTOutput;        // publish temperature and times via mqtt
@@ -48,6 +51,7 @@ class SerialInterface
 {
 public:
     bool initSuccessful{false}; // Was the initialization successful?
+    std::queue<double> TInput{};
     /*
     Instantiate serial connection and maybe 
     check if something is connected.
@@ -57,6 +61,68 @@ public:
         Serial.begin(9600); // can not fail
         // ...               // <-- this can maybe fail?
         initSuccessful = true;
-    };
+    }
+
+    void write_str(String data)
+    {
+        Serial.println(data); 
+    }
+
+    void handleSerial()
+    {   
+        String received;
+        String prefix;
+        String content;
+        while (Serial.available() > 0){
+            received = Serial.readString();
+            prefix = received.substring(0, 4);
+            content = received.substring(4);
+            if (prefix == "data") {
+                // add temperature data
+                TInput.push(std::stod(content.c_str()));  // convert string to double
+                Serial.println("Added "+content+" to the temperature queue.");
+            }
+            else if (prefix == "comm") {
+                // execute command
+                
+            }
+        };
+    }
+};
+
+/*
+Provide simulated temperature values via 
+serial input.
+*/
+class SerialTempInput
+{   
+    private:
+    SerialInterface serial;
+    public:
+    SerialTempInput(SerialInterface &SerialIF)
+    {
+        serial = SerialIF;
+    }
+    SerialTempInput()
+    {
+
+    }
+
+    double getTemperature() 
+    {   
+        double temp;
+        if (serial.TInput.empty())
+        {
+            temp = 25.5;
+        }
+        else
+        {
+            temp = serial.TInput.back();
+            serial.TInput.pop();
+        
+        }
+        return temp;
+    }
+
 };
 #endif
